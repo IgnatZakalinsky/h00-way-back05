d1e05edb5cebc42373d80d4dbb80bf90db5180a7
neko <ai73a@yandex.ru>
2024-04-02 14:48:17 +0300

    endless work

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 let counter = 0
-console.log(counter)
+
+setInterval(() => {
+    counter++
+    console.log(counter)
+}, 1000)
 
 // terminal:
 
-// убедиться в правильности пути в терминале (PS D:\world-tree\i1-work\programming\js\it-incub\final\back\sprint01\s01-hw00\h08-back-samurai-way\back05> )
-
-// node -v (версия ноды) (02.04.2024) v18.18.0
-
 // node server.js
+
+// ctrl+c // for stop (02.04.2024)
