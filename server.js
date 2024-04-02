6a72f1a192808b600350c3aa0fe7d6d74dc96646
neko <ai73a@yandex.ru>
2024-04-02 15:02:50 +0300

    many urls

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
+    switch (req.url) {
+        case '/': { // http://localhost:3000/
+            console.log(1)
+            break
+        }
+        case '/2': { // http://localhost:3000/2
+            console.log(2)
+            break
+        }
+        default: { // http://localhost:3000/favicon.ico
+            console.log(req.url)
+            console.log(404)
+        }
+    }
+
     res.write('hi!')
     res.end()
 })
@@ -11,6 +26,7 @@ server.listen(3000)
 
 // node server.js
 
-// http://localhost:3000/ // (02.04.2024)
+// http://localhost:3000/
 
-// ctrl+c // for stop 
+// ctrl+c // for stop
+// ctrl+l // for clear terminal (02.04.2024)
