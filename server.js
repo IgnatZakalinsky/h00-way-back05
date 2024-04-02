cc6af80aa8edd8d324574f8bca2c70ea16646865
neko <ai73a@yandex.ru>
2024-04-02 14:43:59 +0300

    run simple code

---
+++ server.js
@@ -0,0 +1,10 @@
+let counter = 0
+console.log(counter)
+
+// terminal:
+
+// убедиться в правильности пути в терминале (PS D:\world-tree\i1-work\programming\js\it-incub\final\back\sprint01\s01-hw00\h08-back-samurai-way\back05> )
+
+// node -v (версия ноды) (02.04.2024) v18.18.0
+
+// node server.js
